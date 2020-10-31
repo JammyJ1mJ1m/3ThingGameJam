99a6b0c23c4010204ef87139046b6e7d6c0c839e
JammyJ1mJ1m <52784821+JammyJ1mJ1m@users.noreply.github.com>
2020-10-31 11:33:41 +0000

    deleted example stuff

---
+++ SpiderSim/Assets/Scripts/floorCheckRaycast.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class floorCheckRaycast : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public Rigidbody rb;
+    public Transform hoverpoint;
+    public Transform spider;
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 fwd = transform.TransformDirection(Vector3.down);
+
+        if (Physics.Raycast(transform.position, fwd, 0.5f))
+            spider.position = new Vector3(spider.position.x, hoverpoint.position.y, spider.position.z);
+            print("There is something in below of the object!");
+
+    }
+}
---
+++ SpiderSim/Assets/Scripts/footGroundChecl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class footGroundChecl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+   
+
+    void FixedUpdate()
+    {
+        RaycastHit hit = new RaycastHit();
+
+        Vector3 fwd = transform.TransformDirection(Vector3.down);
+
+        if (Physics.Raycast(transform.position, fwd, out hit))
+        print("foot sees the floor");
+        print(hit.point);
+
+        Debug.DrawLine(transform.position, hit.point, Color.red);
+        // transform.position = new Vector3(transform.position.x, ControllerColliderHit., transform.position.z);
+
+
+    }
+}

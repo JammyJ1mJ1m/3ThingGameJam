16af41635d997b276d852e4c92db65b571ec3bc6
JammyJ1mJ1m <52784821+JammyJ1mJ1m@users.noreply.github.com>
2020-10-31 12:35:30 +0000

    footCheck

--- SpiderSim/Assets/Scripts/footGroundChecl.cs
+++ SpiderSim/Assets/Scripts/footGroundChecl.cs
@@ -4,26 +4,37 @@ using UnityEngine;
 
 public class footGroundChecl : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    /*
+     * The wanted length for the Raycast.
+     */
+    public float distance = 100f;
+    void Update()
     {
-        
-    }
-   
-
-    void FixedUpdate()
-    {
-        RaycastHit hit = new RaycastHit();
-
-        Vector3 fwd = transform.TransformDirection(Vector3.down);
-
-        if (Physics.Raycast(transform.position, fwd, out hit))
-        print("foot sees the floor");
-        print(hit.point);
-
-        Debug.DrawLine(transform.position, hit.point, Color.red);
-        // transform.position = new Vector3(transform.position.x, ControllerColliderHit., transform.position.z);
-
-
+        /*
+         * Create the hit object.
+         */
+        RaycastHit hit;
+        /*
+         * Cast a Raycast.
+         * If it hits something:
+         */
+        if (Physics.Raycast(transform.position, Vector3.down, out hit, distance))
+        {
+            /*
+             * Set the target location to the location of the hit.
+             */
+            Vector3 targetLocation = hit.point;
+            /*
+             * Modify the target location so that the object is being perfectly aligned with the ground (if it's flat).
+             */
+            targetLocation += new Vector3(0, transform.localScale.y / 2, 0);
+            /*
+             * Move the object to the target location.
+             */
+            transform.position = targetLocation;
+        }
     }
 }
+
+
+

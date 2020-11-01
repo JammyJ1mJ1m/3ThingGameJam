7914c529e4fd142bf1c32c4ca872a2edf07cfb96
JammyJ1mJ1m <52784821+JammyJ1mJ1m@users.noreply.github.com>
2020-11-01 18:42:45 +0000

    misc stuff

---
+++ SpiderSim/Assets/Scripts/keyTriggerEnter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class keyTriggerEnter : MonoBehaviour
+{
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter()
+    {
+        //if (dataFromCollision.gameObject.tag == "key")
+        //{
+            Debug.Log("you triggered the key");
+        SceneManager.LoadScene(sceneBuildIndex: 0);
+
+
+        //}
+    }
+
+}
